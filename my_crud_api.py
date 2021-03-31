ac878279216e86734eed2878617bd595de1ac040
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-03-31 20:10:07 +0100

    Define routes

--- my_crud_api.py
+++ my_crud_api.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def hello_world():
   return 'hello world'
 
+@app.route('/users')
+def users():
+  return 'Welcome back'
+
+@app.route('/movies')
+def movies():
+  return 'Categories'
+
+@app.route('/rentals')
+def rentals():
+  return 'Rent a movie'
+
 if __name__ == '__main__':
   app.run()
