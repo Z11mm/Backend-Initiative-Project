9b1e69767e512c48f2f2889e6e3bd3a4348b424e
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-04-02 18:56:52 +0100

    Add comments

--- my_crud_api.py
+++ my_crud_api.py
@@ -5,14 +5,17 @@ app = Flask(__name__)
 def hello_world():
   return 'hello world'
 
+# users
 @app.route('/users')
 def users():
   return 'Welcome back'
 
+# movies
 @app.route('/movies')
 def movies():
   return 'Categories'
 
+# rentals
 @app.route('/rentals')
 def rentals():
   return 'Rent a movie'
