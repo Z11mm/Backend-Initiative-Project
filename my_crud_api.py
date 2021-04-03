961980b0838d4dd2dc1d6d16492f3048edb9bcd6
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-04-03 09:56:53 +0100

    Install dependencies
    Uninstall and reinstall flask and create new requirements file.

--- my_crud_api.py
+++ my_crud_api.py
@@ -1,24 +1,37 @@
 from flask import Flask
-app = Flask(__name__)
+# from flask_restful import Api, Resource
 
-@app.route('/')
-def hello_world():
-  return 'hello world'
+app = Flask(__name__)
+# api = Api(app)
+
+# class User(Resource):
+#   def get(self, user_id):
+#     return {user_id: users[user_id]}
+
+#   def post(self, user_id):
+#     users[user_id] = request.form['data']
+#     return {user_id: users[user_id]},201
+
+# api.add_resource(User, '/users/<string:user_id>')
+
+# @app.route('/')
+# def hello_world():
+#   return 'hello world'
 
 # users
-@app.route('/users')
-def users():
-  return 'Welcome back'
+# @app.route('/users')
+# def users():
+#   return 'Welcome back'
 
 # movies
-@app.route('/movies')
-def movies():
-  return 'Categories'
+# @app.route('/movies')
+# def movies():
+#   return 'Categories'
 
 # rentals
-@app.route('/rentals')
-def rentals():
-  return 'Rent a movie'
+# @app.route('/rentals')
+# def rentals():
+#   return 'Rent a movie'
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
