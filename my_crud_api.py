2fe5dcf4caf0fb85986b83a305e7c47dca7b3b44
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-04-06 13:44:24 +0100

    Add mock json data for users, movies and rentals

--- my_crud_api.py
+++ my_crud_api.py
@@ -1,5 +1,8 @@
-from flask import Flask,request
-from datetime import date
+from flask import Flask, request
+import json
+from users_db import users_db
+from movies_db import movies_db
+from rentals_db import rentals_db
 
 app = Flask(__name__)
 
@@ -10,27 +13,24 @@ def hello_world():
 
 # users
 @app.route('/users', methods=['GET'])
-def users():
-  users = [
-    {
-      'name': "Oma",
-      'age': 24,
-      'gender': 'female',
-      'rentals': 0,
-      'date joined': date.today().strftime('%B %d, %Y')
-    },
-    {
-      'name': "Tunde",
-      'age': 33,
-      'gender': 'male',
-      'rentals': 0,
-      'date joined': date.today().strftime('%B %d, %Y')
-    }
-  ]
-  return {"users": users}
+def get_users():
+  users = json.dumps(users_db)
+  return users
 
 # @app.route('/profile/<id>', methods=['GET'])
 # def get_user(id):
+
+# movies
+@app.route('/movies', methods=['GET'])
+def get_movies():
+  movies = json.dumps(movies_db)
+  return movies
+
+# rentals
+@app.route('/rentals', methods=['GET'])
+def get_rentals():
+  rentals = json.dumps(rentals_db)
+  return rentals
 
 if __name__ == '__main__':
   app.run(debug=True)
