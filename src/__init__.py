b28673a3c61a017c6b1056e2e0a82402e985a1d5
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-05-21 19:07:51 +0100

    Refactor: Reorganize code into blueprints

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
 
 def create_app():
-  app = Flask(__name__)
+    app = Flask(__name__)
+    app.config['FLASK_ENV']='development'
 
-  return app
+    # import routes
+    from .routes import homepage
+
+    # register blueprints
+    app.register_blueprint(homepage)
+
+    return app
