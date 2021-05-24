1d6b2579457d40973b2d5ae6394494922f052158
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-05-24 09:52:06 +0100

    Add config file

---
+++ config.py
@@ -0,0 +1,25 @@
+# Flask configuration variables
+
+from logging import DEBUG
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+class Config:
+    """Set Flask configuration from .env file"""
+    # Base config
+    SECRET_KEY = environ.get('SECRET_KEY')
+
+class DevConfig(Config):
+    FLASK_ENV = 'development'
+    DEBUG = True
+    TESTING = True
+    DATABASE_URI = environ.get('DEV_DATABASE_URI')
+    
+class ProdConfig(Config):
+    FLASK_ENV = 'production'
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = environ.get('PROD_DATABASE_URI')
--- src/__init__.py
+++ src/__init__.py
@@ -4,10 +4,11 @@ def create_app():
     app = Flask(__name__)
     app.config['FLASK_ENV'] = 'development'
 
-    # import routes
-    from .home import views
+    with app.app_context():
+        # import routes
+        from .home import views
 
-    # register blueprints
-    app.register_blueprint(views.homepage)
+        # register blueprints
+        app.register_blueprint(views.homepage)
 
     return app
